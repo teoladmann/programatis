ddac3bac0b2d37e18c602fec7b41e393bed3f498
Timoteo Ladmann <teoladmann@gmail.com>
2020-05-03 19:37:09 -0300

    add Article

---
+++ src/components/Article.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'gatsby';
+
+const Article = ({ edge }) => {
+  return (
+    <Link to={`clase/${edge.node.fields.slug}`}>
+      <article className="article">
+        {edge.node.frontmatter.title}
+      </article>
+    </Link>
+  )
+}
+
+export default Article;
