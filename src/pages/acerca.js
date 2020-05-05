db9534071669fc1bd8159c60ed5277f7c3d148be
Timoteo Ladmann <teoladmann@gmail.com>
2020-05-05 20:05:40 -0300

    feat: add about page

---
+++ src/pages/acerca.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Layout from '../layout/index';
+
+const Acerca = () => {
+  return (
+    <Layout>
+      <h2>¿Qué es Programatis?</h2>
+      <p>Es una plataforma <strong>gratuita</strong> para aprender a programar <strong>en español</strong>.</p>
+    </Layout>
+  )
+}
+
+export default Acerca;
